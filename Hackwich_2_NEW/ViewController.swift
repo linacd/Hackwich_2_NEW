cb949a815132adf1348e3131f28bf30c5fe2aaf7
linacd <linacd@hawaii.edu>
2023-09-12 20:34:52 -1000

    I have finished parts 1-8

--- Hackwich_2_NEW/ViewController.swift
+++ Hackwich_2_NEW/ViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Part 8 Declaring 3 strings vars
+    
+    var newvar1 = "Ive created a new var1"
+    
+    var newvar2 = "Ive created a second var2"
+    
+    //Declaring the third var as a empty string
+    var newvar3 = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Assigning the first two strings into a string concatenation
+        newvar3 = newvar1 + newvar2
+        
+        print (newvar3)
+        //Finished Part 8 (1-3)
     }
+    
+    @IBAction func NewButton(_ sender: Any) {
+    }
+    // Do any additional setup after loading the view.
+    //Parts 1-7
+
 
 
     @IBAction func pressMeButton(_ sender: Any) {
@@ -20,4 +41,4 @@ class ViewController: UIViewController {
         }
     
 }
-
+//Completed parts 1-7
